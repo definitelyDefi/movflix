7e81b44eb188a4c93ba918fc5a133c779677c75f
definitelyDefi <yuichirohaky227@gmail.com>
2023-02-15 17:27:31 +0100

    Updates

---
+++ precache-manifest.bcb0a0558e2d42ab01d4968affce3ddd.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "5306f63360835366b2c6",
+    "url": "/movflix/static/css/main.518d725c.chunk.css"
+  },
+  {
+    "revision": "5306f63360835366b2c6",
+    "url": "/movflix/static/js/main.5306f633.chunk.js"
+  },
+  {
+    "revision": "d8ef2d5e1dd0bdb14688",
+    "url": "/movflix/static/css/1.78b245c1.chunk.css"
+  },
+  {
+    "revision": "d8ef2d5e1dd0bdb14688",
+    "url": "/movflix/static/js/1.d8ef2d5e.chunk.js"
+  },
+  {
+    "revision": "6918e23c6c8df685206d",
+    "url": "/movflix/static/js/runtime~main.6918e23c.js"
+  },
+  {
+    "revision": "f04decbee1bc5b0d3c412bf107b1c74e",
+    "url": "/movflix/static/media/background.f04decbe.jpg"
+  },
+  {
+    "revision": "a66aedf937e9ebc8e53f982041cd6700",
+    "url": "/movflix/static/media/arrow-right-circle.a66aedf9.svg"
+  },
+  {
+    "revision": "6719c6af8b79c92bf83347cddb8816fd",
+    "url": "/movflix/static/media/arrow-left-circle.6719c6af.svg"
+  },
+  {
+    "revision": "7108494729f1998a285578b65d113092",
+    "url": "/movflix/static/media/imdb.71084947.png"
+  },
+  {
+    "revision": "cec41d1a5d2d6003a17b5a2f984c6cfc",
+    "url": "/movflix/static/media/avatar_author.cec41d1a.png"
+  },
+  {
+    "revision": "6d312dffe8b13129b1b637ee58634397",
+    "url": "/movflix/index.html"
+  }
+];
